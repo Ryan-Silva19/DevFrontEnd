a9b72120bb275d48b8f21e18f7743909a826ff20
Ryan-Silva19 <ryanferreiradasilva2006@gmail.com>
2025-06-30 15:52:49 -0300

    exercise 7

---
+++ Aula30-06/exercise7.js
@@ -0,0 +1,7 @@
+// 7 - Crie um objeto aluno com as propriedades nome, idade e curso. Mostre o nome do aluno no console.
+let student = {
+    name: "Ryan",
+    age: 19,
+    course: "Desenvolvedor Front-End"
+}
+console.log(student.name)
