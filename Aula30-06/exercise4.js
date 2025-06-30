6dfc88ab68d7bb530a6de9b0195fc2af30e10cdb
Ryan-Silva19 <ryanferreiradasilva2006@gmail.com>
2025-06-30 14:41:34 -0300

    exercise 4

---
+++ Aula30-06/exercise4.js
@@ -0,0 +1,4 @@
+// 4 - Crie duas variáveis com preços de produtos e mostre a soma deles no console.
+let ps5Price = 5000;
+let shower = 120;
+console.log("Preço da sua compra: R$",ps5Price + shower)
