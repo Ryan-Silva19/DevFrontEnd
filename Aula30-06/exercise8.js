560de9734d125b9a97e7c68573f0cceb1b3af30b
Ryan-Silva19 <ryanferreiradasilva2006@gmail.com>
2025-06-30 15:55:25 -0300

    exercise 8

---
+++ Aula30-06/exercise8.js
@@ -0,0 +1,7 @@
+// 8 - Crie um objeto livro com titulo, autor e paginas. Mostre uma frase com essas informações no console.
+let book = {
+    title: "Harry Potter e a pedra filosofal",
+    author: "J.K Rowling",
+    pages: 264
+};
+console.log("informações do livro '"+ book.title + "'\n" + "Autor(a): " + book.author + "\n" + "páginas do livro:", book.pages);
