149ccac4548cdb7508ba765f1a06005b5d1cd107
Ryan <ryanferreiradasilva2006@gmail.com>
2025-07-02 14:13:29 -0300

    feat: operadores de comparação

---
+++ Aula02-07/atividade3.js
@@ -0,0 +1,9 @@
+// 1 - Crie duas variáveis com idades e compare:
+// - idade1 é maior que idade2?
+// - Elas têm a mesma idade (usando ===)?  
+// - As idades são diferentes?
+let age = 19;
+let age2 = 21;
+console.log(`A idade um é maior que a idade 2 ?\n ${age > age2}`);
+console.log(`As idades são parecidas ?\n ${age == age2}`);
+console.log(`As idades são diferentes ?\n ${age != age2}`);
