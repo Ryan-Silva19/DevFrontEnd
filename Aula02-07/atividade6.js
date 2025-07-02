de141ce8338d4f919d26653763a43dbef3799555
Ryan <ryanferreiradasilva2006@gmail.com>
2025-07-02 16:09:37 -0300

    feat: add age verification logic

---
+++ Aula02-07/atividade6.js
@@ -0,0 +1,11 @@
+// Exercício 2 - Verificando maioridade
+// Crie uma variável idade.  
+// Se for maior ou igual a 18, exiba "Maior de idade".  
+// Senão, exiba "Menor de idade".
+
+let age = prompt("insira sua idade meu caro usuário: ");
+if (age >= 18) {
+    alert("Você é maior de idade");
+} else {
+    alert("Você é menor de idade");
+}
