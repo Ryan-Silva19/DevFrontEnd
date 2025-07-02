c4fe4df6c34a7b14eb49098796a5da65630565a7
Ryan <ryanferreiradasilva2006@gmail.com>
2025-07-02 13:37:28 -0300

    feat: operadores lógicos

---
+++ Aula02-07/atividade1.js
@@ -0,0 +1,17 @@
+// 1 - Crie duas variáveis com dois números e mostre:
+// - A soma
+// - A subtração
+// - A multiplicação
+// - A divisão
+// - O resta da divisão
+
+let a = 10;
+let b = 25;
+
+console.log(a+b);
+
+console.log(b-a);
+
+console.log(a*b);
+
+console.log(b/a);
