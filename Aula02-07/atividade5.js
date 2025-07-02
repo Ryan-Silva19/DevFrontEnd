1bd15b73b36636182cd53506ea060568437a622b
Ryan <ryanferreiradasilva2006@gmail.com>
2025-07-02 16:08:46 -0300

    feat: add grade evaluation logic

---
+++ Aula02-07/atividade5.js
@@ -0,0 +1,11 @@
+// Exercício 1 - Verificando Nota Escolar
+// Crie uma variável nota e atribua um valor entre 0 e 10.  
+// Se nota >= 5, exiba "Aprovado!".
+// Se nota < 5, exiba "Reprovado".
+
+let nota = prompt("insira sua nota escolar: ");
+if(nota >=5){
+    alert("Aprovado!");
+}else{
+    alert("Reprovado com sucesso!");
+}
