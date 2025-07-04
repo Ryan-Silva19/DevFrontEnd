efb7732ecd139e2d81db4e95cd8752b3f667dc62
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-07-04 16:37:01 -0300

    refactor: multiplication table

--- Aula04-07/atividade12.js
+++ Aula04-07/atividade12.js
@@ -1,5 +1,5 @@
 // Exercício 4 - Tabuada do 5
 // Crie um for que mostre a tabuada do 5, do 5 x 1 até o 5 x 10.  
-for(let i = 0; i <= 50; i+=5){
-    console.log(i);
+for(let i = 0; i <= 10; i++){
+    console.log(`5 X ${i} = ${5 * i}`);
 }
