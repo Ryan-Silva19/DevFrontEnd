c43a9c135f5d8f04582fcad93341dd1037ae2656
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-07-04 16:37:30 -0300

    feat: add loop to sum numbers from 1 to 100

---
+++ Aula04-07/atividade13.js
@@ -0,0 +1,8 @@
+// Exercício 5 - Somar os números de 1 até 100
+// Some todos os números de 1 a 100 usando for.
+// No final, mostre o total no console. (Resposta: 5050)
+let total = 0;
+for(let i = 0; i <= 100; i++){
+    total += i;
+}
+console.log(`${total}`);
